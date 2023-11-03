ceb7982efbed1527ae475024f19d5a82b7ad0a09
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-02 22:09:38 -0700

    AuthLayout and ShadCN

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
-const AuthLayout = () => {
+
+export default function AuthLayout() {
+  const isAuthenticated = false
+
   return (
-    <div>AuthLayout</div>
-  )
-}
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
 
-export default AuthLayout
+          <img
+            src="/assets/images/download.jpg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+}
